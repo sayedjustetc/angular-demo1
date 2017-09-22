1713f4d766e0ff542047bee95d390e231ba6c192
Sayed Ahmed <sayedahmed@Sayeds-MacBook-Pro.local>
2017-09-22 18:08:12 -0400

    changed the app.component.ts, changed template, changed export variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: ` <div>
+  <h1>{{appTitle}}</h1>
+  <div>To Tutorials Point</div>
+</div> `
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent {
+  appTitle: string = 'Welcome';
+}
+
+
