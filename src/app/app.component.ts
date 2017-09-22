942b19371d729b163db2501deed776e21f823d67
Sayed Ahmed <sayedahmed@Sayeds-MacBook-Pro.local>
2017-09-22 18:13:08 -0400

    modified app.component.ts and pointed to a template file app.component.html - instead of writing the template html inside app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'my-app',
-  template: ` <div>
-  <h1>{{appTitle}}</h1>
-  <div>To Tutorials Point</div>
-</div> `
+@Component ({
+   selector: 'my-app',
+   templateUrl: 'app/app.component.html'  
 })
 
 export class AppComponent {
-  appTitle: string = 'Welcome';
+   appTitle: string = 'Welcome';
 }
-
-
